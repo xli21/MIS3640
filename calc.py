ac084d1e7e39e3b8675d821fc95b0ce6f6aa333b
xli21 <xli12@babson.edu>
2017-09-07 13:41:24 -0400

    Activites 2

--- calc.py
+++ calc.py
@@ -3,3 +3,18 @@ print(42 * 60 + 42)
 print(10 * 1.6)
 print(16 / 2565)
 print(16 / (42.7 / 60))
+print(36 + 7)
+
+#Question 1
+pi = 3.14
+r = 5
+print((4 / 3) * pi * r ** 3)
+
+#Question 2
+print('Price:$',24.95, 'Discount: %d%%' % 40, 'Shipping Cost: $3 first, and $.75 for addition')
+print('The cost for 60 copys are %.2f' % (24.95 * (1 - 0.4) * 60 + 3 + 0.75 * 59))
+
+#Question 3
+
+#Question 4
+print('My grade has increased %.1f%%' % ((89 - 82) / 82))
---
+++ variable.py
@@ -0,0 +1,23 @@
+print('hello, world!')
+print('Hey Jude', 'don\'t make it bad')
+print ('The total number of overall medals in Rio 2016 is', 46 + 37 + 38)
+print ('46 + 37 + 38 =',46 + 37 + 38)
+
+
+
+
+
+message = 'I learned something Cool!'
+
+n = 100
+pi = 3.14
+
+print('Naah, na na nanana naah, nanana naah, hey Jude. ' * 10)
+
+
+print('Hello, %s' % 'world')
+
+print('Today is %2d-%02d.' % (9, 5))
+print('Pi equals %.2f.' % 3.1415926)
+print('Age: %s. Gender: %s' % (20, True))
+print('Growth rate: %d %%' % 7)
